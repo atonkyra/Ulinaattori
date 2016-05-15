15b9e7a87aaeb76265065a21d0eaf513e3f47f72
Antti Tönkyrä <daedalus@pingtimeout.net>
2016-05-15 14:29:42 +0300

    yay

--- Ulinaattori/Plugins/CUEPlugin.cs
+++ Ulinaattori/Plugins/CUEPlugin.cs
@@ -34,23 +34,73 @@ namespace Ulinaattori.Plugins {
                 throw new WrapperException("No keyboard found");
         }
 
-        private void executeEvent() {
+        private int getCosT(double innerFactor, float t) {
+            return (int)Math.Round(innerFactor * Math.Cos(t), MidpointRounding.AwayFromZero);
+        }
+
+        private void executeMKFadeAlert(string topic) {
+            List<CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId> kids = new List<CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId>();
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.Escape);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F1);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F2);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F3);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F4);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F5);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F6);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F7);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F8);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F9);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F10);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F11);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.F12);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.PrintScreen);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.ScrollLock);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.PauseBreak);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.Stop);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.ScanPreviousTrack);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.PlayPause);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.ScanNextTrack);
+            float t = 0;
+            while(!clearFlag) {
+                float tb = t;
+                foreach(var key in kids) {
+                    System.Drawing.Color c = System.Drawing.Color.FromArgb(128 + getCosT(127.0, tb), 0, 16 + getCosT(15.0, tb));
+                    this.kb[key].Led.Color = c;
+                    tb += 0.2f;
+                }
+                this.kb.Update();
+                t-=0.05f;
+                Thread.Sleep(6);
+            }
+            Thread.Sleep(100);
+        }
+
+        private void executeEvent(string topic) {
             lock(this) {
                 if(eventRunning) return;
-                eventRunning = true;
+                this.eventRunning = true;
             }
-            /*List<CUE.NET.Devices.Keyboard.Keys.CorsairKey> kstrs = new List<CUE.NET.Devices.Keyboard.Keys.CorsairKey>();
+
+            this.executeMKFadeAlert(topic);
+
+            CueSDK.Reinitialize();
+            this.eventRunning = false;
+            this.clearFlag = false;
+        }
+            /*
+            List<CUE.NET.Devices.Keyboard.Keys.CorsairKey> kstrs = new List<CUE.NET.Devices.Keyboard.Keys.CorsairKey>();
             foreach(var key in kb.Keys) {
                 string kstr = key.KeyId.ToString();
-                if(kstr != "F" && kstr.StartsWith("F")) {
+                if(true || kstr != "F" && kstr.StartsWith("F")) {
                     kstrs.Add(key);
                 }
-            }
-            kstrs.Sort(new NumStrCmp());
+            }*/
+            //kstrs.Sort(new NumStrCmp());
+            /*
             foreach(var key in kstrs) {
                 this.kb[key.KeyId].Led.Color = System.Drawing.Color.Red;
                 this.kb.Update();
-                Thread.Sleep(100);
+                Thread.Sleep(50);
             }
             System.Drawing.Color c = System.Drawing.Color.Red;
             for(int i = 0; i < 360; i++) {
@@ -65,35 +115,32 @@ namespace Ulinaattori.Plugins {
                 this.kb.Update();
                 Thread.Sleep(100);
             }*/
-            List<CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId> kids = new List<CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId>();
+
+            /*List<CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId> kids = new List<CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId>();
             kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.Stop);
             kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.ScanPreviousTrack);
             kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.PlayPause);
-            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.ScanNextTrack);
-            System.Drawing.Color c = System.Drawing.Color.Red;
-            for(int i = 0; i < 360; i++) {
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.ScanNextTrack);*/
+            //System.Drawing.Color c = System.Drawing.Color.Red;
+            /*for(int i = 0; i < 360; i++) {
                 if(clearFlag) break;
                 if(c == System.Drawing.Color.Red)
                     c = System.Drawing.Color.Black;
                 else
                     c = System.Drawing.Color.Red;
-                /*foreach(var key in kstrs) {
+                foreach(var key in kstrs) {
                     this.kb[key.KeyId].Led.Color = c;
-                }*/
+                }
                 foreach(var key in kids) {
                     this.kb[key].Led.Color = c;
                 }
                 this.kb.Update();
                 Thread.Sleep(100);
-            }
-
-            CueSDK.Reinitialize();
-            this.eventRunning = false;
-            this.clearFlag = false;
-        }
+            }*/
+        //}
 
         public void onMessage<T>(string topic, T payload) {
-            Task.Run(() => executeEvent());
+            Task.Run(() => executeEvent(topic));
         }
 
         public void raiseClearFlag() {
