532bd35742f440fed58dd709db2f638e68da800b
Antti Tönkyrä <daedalus@pingtimeout.net>
2016-05-15 05:19:40 +0300

    more stuff

--- Ulinaattori/Plugins/CUEPlugin.cs
+++ Ulinaattori/Plugins/CUEPlugin.cs
@@ -39,14 +39,11 @@ namespace Ulinaattori.Plugins {
                 if(eventRunning) return;
                 eventRunning = true;
             }
-            List<CUE.NET.Devices.Keyboard.Keys.CorsairKey> kstrs = new List<CUE.NET.Devices.Keyboard.Keys.CorsairKey>();
+            /*List<CUE.NET.Devices.Keyboard.Keys.CorsairKey> kstrs = new List<CUE.NET.Devices.Keyboard.Keys.CorsairKey>();
             foreach(var key in kb.Keys) {
                 string kstr = key.KeyId.ToString();
                 if(kstr != "F" && kstr.StartsWith("F")) {
                     kstrs.Add(key);
-                    /*this.kb[key.KeyId].Led.Color = System.Drawing.Color.Red;
-                    Thread.Sleep(10);
-                    this.kb.Update();*/
                 }
             }
             kstrs.Sort(new NumStrCmp());
@@ -67,7 +64,29 @@ namespace Ulinaattori.Plugins {
                 }
                 this.kb.Update();
                 Thread.Sleep(100);
+            }*/
+            List<CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId> kids = new List<CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId>();
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.Stop);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.ScanPreviousTrack);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.PlayPause);
+            kids.Add(CUE.NET.Devices.Keyboard.Enums.CorsairKeyboardKeyId.ScanNextTrack);
+            System.Drawing.Color c = System.Drawing.Color.Red;
+            for(int i = 0; i < 360; i++) {
+                if(clearFlag) break;
+                if(c == System.Drawing.Color.Red)
+                    c = System.Drawing.Color.Black;
+                else
+                    c = System.Drawing.Color.Red;
+                /*foreach(var key in kstrs) {
+                    this.kb[key.KeyId].Led.Color = c;
+                }*/
+                foreach(var key in kids) {
+                    this.kb[key].Led.Color = c;
+                }
+                this.kb.Update();
+                Thread.Sleep(100);
             }
+
             CueSDK.Reinitialize();
             this.eventRunning = false;
             this.clearFlag = false;
@@ -75,7 +94,6 @@ namespace Ulinaattori.Plugins {
 
         public void onMessage<T>(string topic, T payload) {
             Task.Run(() => executeEvent());
-            Console.WriteLine("JEE");
         }
 
         public void raiseClearFlag() {
